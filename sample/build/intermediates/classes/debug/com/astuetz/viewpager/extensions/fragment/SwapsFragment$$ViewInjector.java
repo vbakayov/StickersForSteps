a5ad46c502f08204f6382e29b90c35b0ca8215b1
vbakayov <viktorbakayov@gmail.com>
2015-10-24 15:29:59 +0100

    bluetooth permisions

---
+++ sample/build/intermediates/classes/debug/com/astuetz/viewpager/extensions/fragment/SwapsFragment$$ViewInjector.java
@@ -0,0 +1,17 @@
+// Generated code from Butter Knife. Do not modify!
+package com.astuetz.viewpager.extensions.fragment;
+
+import android.view.View;
+import butterknife.ButterKnife.Finder;
+
+public class SwapsFragment$$ViewInjector {
+  public static void inject(Finder finder, final com.astuetz.viewpager.extensions.fragment.SwapsFragment target, Object source) {
+    View view;
+    view = finder.findRequiredView(source, 2131427425, "field 'textView'");
+    target.textView = (android.widget.TextView) view;
+  }
+
+  public static void reset(com.astuetz.viewpager.extensions.fragment.SwapsFragment target) {
+    target.textView = null;
+  }
+}
